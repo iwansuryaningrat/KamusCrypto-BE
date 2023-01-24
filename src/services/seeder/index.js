67e85e86179d86fe622098669db2115aecbda047
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-01-25 03:00:12 +0700

    Update Seeder

--- src/services/seeder/index.js
+++ src/services/seeder/index.js
@@ -7,10 +7,10 @@ import usersSeederFunction from "./users.seeder.js";
 import deleteAll from "./delete.seeder.js";
 
 // Run Seeder
-// const userSeeder = await usersSeederFunction();
-// console.log(userSeeder);
-const deleteAllSeeder = await deleteAll();
-console.log(deleteAllSeeder);
+const userSeeder = await usersSeederFunction();
+console.log(userSeeder);
+// const deleteAllSeeder = await deleteAll();
+// console.log(deleteAllSeeder);
 
 //   end task
 process.exit();
