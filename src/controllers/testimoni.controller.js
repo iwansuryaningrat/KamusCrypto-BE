566e20dea375d297bc511a74b531499ad8176a8e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-01-28 01:54:33 +0700

    Update Testimoni Controller

--- src/controllers/testimoni.controller.js
+++ src/controllers/testimoni.controller.js
@@ -1,7 +1,7 @@
 import db from "../models/index.js";
 const Testimoni = db.testimoni;
 
-import dataCounter from "./function/dataCounter.function.js";
+import dataCounter from "../helpers/dataCounter.js";
 
 import mongoose from "mongoose";
 const ObjectId = mongoose.Types.ObjectId;
