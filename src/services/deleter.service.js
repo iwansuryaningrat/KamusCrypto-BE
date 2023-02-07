895039c17c38e9b65101b798c2e7347ffe7a4ae1
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-02-07 16:53:12 +0700

    Update Deleter Helper

--- src/services/deleter.service.js
+++ src/services/deleter.service.js
@@ -2,11 +2,29 @@
 import connect from "./db.connect.service.js";
 connect();
 import db from "../models/index.js";
+const Certificate = db.certificate;
+const Liveclass = db.liveclass;
 const LiveclassTransactions = db.liveclassTransactions;
+const MembershipTransactions = db.membershipTransactions;
+const Messages = db.messages;
+const News = db.news;
+const Plans = db.plans;
+const Playlists = db.playlists;
+const PlaylistsTracking = db.playlistsTracking;
+const Referrals = db.referrals;
+const RepliedMessages = db.repliedMessages;
+const Services = db.services;
+const Subscribers = db.subscribers;
+const Teams = db.teams;
+const Testimoni = db.testimoni;
+const Users = db.users;
+const Videos = db.videos;
+const Vouchers = db.vouchers;
+const Watchlist = db.watchlist;
 
-const deleter = async () => {
+const deleter = async (Model) => {
   try {
-    await LiveclassTransactions.deleteMany({});
+    await Model.deleteMany({});
     return true;
   } catch (error) {
     return false;
@@ -14,7 +32,7 @@ const deleter = async () => {
 };
 
 const deleteData = async () => {
-  const isDeleted = await deleter();
+  const isDeleted = await deleter(Playlists);
   if (isDeleted) {
     return "Deleted";
   } else {
