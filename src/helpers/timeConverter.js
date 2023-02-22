9be773034b96fb7317fc04f0b9ed2802cf118e7c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-02-22 11:19:46 +0700

    Create Documentation for Time Converter Helper

--- src/helpers/timeConverter.js
+++ src/helpers/timeConverter.js
@@ -1,3 +1,9 @@
+/**
+ * It takes a date object and returns a string in the format of `YYYY-MM-DD HH:MM:SS +HH:MM` where
+ * `+HH:MM` is the timezone offset.
+ * @param date - The date to convert.
+ * @returns A string in the format of: YYYY-MM-DD HH:MM:SS +/-HH:MM
+ */
 const timeConvert = (date) => {
   const d = new Date(date);
   const offset = d.getTimezoneOffset();
