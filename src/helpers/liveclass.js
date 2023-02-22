9794f8ce49577030d2fefc5f31a7a5ab7a8d4c50
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-02-22 11:19:18 +0700

    Create Documentation for Liveclass Helper

--- src/helpers/liveclass.js
+++ src/helpers/liveclass.js
@@ -1,7 +1,15 @@
+/* Importing the liveclass model from the index.js file in the models folder. */
 import db from "../models/index.js";
 const Liveclass = db.liveclass;
 
 // Add User to Liveclass's Participant (Done)
+/**
+ * It takes a userID and a liveclassId, finds the liveclass, increments the participantsCount, adds the
+ * userID to the participantsList, and saves the liveclass.
+ * @param userID - the user's ID
+ * @param liveclassId - the id of the liveclass
+ * @returns A promise.
+ */
 const addParticipant = (userID, liveclassId) => {
   const liveclass = Liveclass.findById(liveclassId)
     .then((liveclass) => {
