f717c5ad45858a7a15ac32fa346073f320cb6aa1
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-02-22 11:19:55 +0700

    Create Documentation for Videos Helper

--- src/helpers/videos.js
+++ src/helpers/videos.js
@@ -1,7 +1,30 @@
+/* Importing the database and the videos collection from the database. */
 import db from "../models/index.js";
 const Videos = db.videos;
 
 // Update Video URL
+/**
+ * It takes in a videoId, and four urls, and updates the video with the given videoId with the given
+ * urls
+ * @param videoId - The id of the video that you want to update.
+ * @param url1080 -
+ * https://r1---sn-4g5e6n7s.googlevideo.com/videoplayback?expire=1598420000&ei=_Q-XX9_4J4yX1wK-6Z-gCw&
+ * @param url720 -
+ * https://r2---sn-4g5edn7l.googlevideo.com/videoplayback?expire=1598420000&ei=_Q-XX9_4J4yX1wK-6Z-gCw&ip
+ * @param url480 -
+ * https://r1---sn-4g5e6n7s.googlevideo.com/videoplayback?expire=1598420000&ei=_Q-XX9_4J4yX1wK-6Z-gCw&
+ * @param url360 -
+ * https://r1---sn-4g5e6n7s.googlevideo.com/videoplayback?expire=1598420100&ei=_Q-XX9_4J4yX1wK-6Z-gCw&
+ * @returns {
+ *   "url": [
+ *     {
+ *       "quality": "1080p",
+ *       "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+ *     },
+ *     {
+ *       "quality": "720p",
+ *       "url": "https://www.youtube.com/
+ */
 const updateVideoUrl = async (videoId, url1080, url720, url480, url360) => {
   return await Videos.findByIdAndUpdate(videoId, {
     url: [
@@ -32,6 +55,12 @@ const updateVideoUrl = async (videoId, url1080, url720, url480, url360) => {
 };
 
 // Update Video Views
+/**
+ * It takes a videoId, finds the video in the database, increments the views by 1, and returns true if
+ * the update was successful.
+ * @param videoId - The id of the video you want to update the views for.
+ * @returns a promise.
+ */
 const updateVideoViews = async (videoId) => {
   const video = await Videos.findById(videoId);
   if (video) {
@@ -51,6 +80,12 @@ const updateVideoViews = async (videoId) => {
 };
 
 // Update Video Likes
+/**
+ * It takes a videoId, finds the video in the database, increments the likes by 1, and returns true if
+ * the update was successful.
+ * @param videoId - The id of the video that is being liked.
+ * @returns a promise.
+ */
 const updateVideoLikes = async (videoId) => {
   const video = await Videos.findById(videoId);
   if (video) {
@@ -70,6 +105,12 @@ const updateVideoLikes = async (videoId) => {
 };
 
 // Update Video Dislikes
+/**
+ * It takes a videoId, finds the video in the database, increments the dislikes by 1, and returns true
+ * if the update was successful.
+ * @param videoId - The id of the video that is being liked.
+ * @returns a promise.
+ */
 const updateVideoDislikes = async (videoId) => {
   const video = await Videos.findById(videoId);
   if (video) {
@@ -88,6 +129,12 @@ const updateVideoDislikes = async (videoId) => {
   }
 };
 
+/**
+ * It takes a videoId and playlistId and updates the video with the playlistId.
+ * @param videoId - The id of the video you want to add to the playlist
+ * @param playlistId - 5f0f8f8f8f8f8f8f8f8f8f8f
+ * @returns The result of the promise.
+ */
 const updatePlaylistVideo = async (videoId, playlistId) => {
   return await Videos.findByIdAndUpdate(videoId, {
     playlist: playlistId,
