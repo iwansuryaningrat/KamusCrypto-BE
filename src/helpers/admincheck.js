8dc5dbcd12294b9b4cff87e9f5bd7222101542f7
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-02-22 11:18:59 +0700

    Create Documentation for Admin Check Helper

--- src/helpers/admincheck.js
+++ src/helpers/admincheck.js
@@ -1,6 +1,12 @@
+/* Importing the database and the users table. */
 import db from "../models/index.js";
 const Users = db.users;
 
+/**
+ * It checks if the user is an admin or not.
+ * @param id - The id of the user you want to check
+ * @returns A promise.
+ */
 const adminCheck = async (id) => {
   const response = await Users.findById(id)
     .then((data) => {
