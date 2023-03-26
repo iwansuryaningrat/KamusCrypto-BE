d5c048135b9f85fd4eab8ec8e66e214851ce1140
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-26 13:35:22 +0700

    Update Image Uploader

--- src/configs/imageUploader.config.js
+++ src/configs/imageUploader.config.js
@@ -1,15 +1,42 @@
 import multer from "multer";
+import aws from "aws-sdk";
+import multerS3 from "multer-s3";
+import shortid from "shortid";
+
+// Set S3 endpoint to DigitalOcean Spaces
+const spacesEndpoint = new aws.Endpoint(process.env.DO_SPACES_ENDPOINT);
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: process.env.DO_SPACES_KEY,
+  secretAccessKey: process.env.DO_SPACES_SECRET,
+});
 
 // Image Uploader Setup
 /* Creating a storage object for multer to use. */
-const imageStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./assets/images");
-  },
-  filename: (req, file, cb) => {
+// const imageStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./assets/images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       new Date().getTime() + "-" + file.originalname.replace(/\s/g, "-")
+//     );
+//   },
+// });
+
+const imageStorage = multerS3({
+  s3: s3,
+  bucket: "kamuscrypto",
+  acl: "public-read",
+  key: function (req, file, cb) {
+    console.log(file);
     cb(
       null,
-      new Date().getTime() + "-" + file.originalname.replace(/\s/g, "-")
+      "images/" +
+        shortid.generate() +
+        "-" +
+        file.originalname.replace(/\s/g, "-")
     );
   },
 });
