f808eea24f1d91e158de2d54c388679f01a185d9
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-04-16 21:41:36 +0700

    Update Seeder

--- src/services/seeder/index.js
+++ src/services/seeder/index.js
@@ -4,22 +4,24 @@ connect();
 
 // Import Seeder
 import usersSeederFunction from "./users.seeder.js";
-import deleteAll from "./delete.seeder.js";
 import seedPlans from "./plans.seeder.js";
 import seedTestimoni from "./testimoni.seeder.js";
 import seedTeams from "./teams.seeder.js";
+import seedNews from "./blog.seeder.js";
 
 // Run Seeder
-const userSeeder = await usersSeederFunction();
-console.log(userSeeder);
-const deleteAllSeeder = await deleteAll();
-console.log(deleteAllSeeder);
+if (process.env.NODE_ENV !== "production") {
+  const userSeeder = await usersSeederFunction();
+  console.log(userSeeder);
+}
 const plansSeeder = await seedPlans();
 console.log(plansSeeder);
 const testimoniSeeder = await seedTestimoni();
 console.log(testimoniSeeder);
 const teamsSeeder = await seedTeams();
 console.log(teamsSeeder);
+const newsSeeder = await seedNews();
+console.log(newsSeeder);
 
 //   end task
 process.exit();
