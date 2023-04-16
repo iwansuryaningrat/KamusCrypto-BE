3218327ae35519321bbe18b45e1ee9296985fdc4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-04-16 23:49:18 +0700

    Update index of seeder

--- src/services/seeder/index.js
+++ src/services/seeder/index.js
@@ -3,17 +3,12 @@ import connect from "../db.connect.service.js";
 connect();
 
 // Import Seeder
-import usersSeederFunction from "./users.seeder.js";
 import seedPlans from "./plans.seeder.js";
 import seedTestimoni from "./testimoni.seeder.js";
 import seedTeams from "./teams.seeder.js";
 import seedNews from "./blog.seeder.js";
 
 // Run Seeder
-if (process.env.NODE_ENV !== "production") {
-  const userSeeder = await usersSeederFunction();
-  console.log(userSeeder);
-}
 const plansSeeder = await seedPlans();
 console.log(plansSeeder);
 const testimoniSeeder = await seedTestimoni();
