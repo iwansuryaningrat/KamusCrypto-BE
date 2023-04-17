5e8a8873729dffec900e768d1c916905c01e60d4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-04-18 01:11:14 +0700

    Update Notif

--- src/routes/notifications.routes.js
+++ src/routes/notifications.routes.js
@@ -1,9 +1,15 @@
-import notificationController from "../controllers/notification.controller.js";
+import {
+  notificationController,
+  notificationrecurring,
+  notificationpay,
+} from "../controllers/notification.controller.js";
 import Express from "express";
 const router = Express.Router();
 
 const notificationsRouter = (app) => {
   router.post("/", notificationController);
+  router.post("/recurring", notificationrecurring);
+  router.post("/pay", notificationpay);
 
   app.use("/v1/notifications", router);
 };
